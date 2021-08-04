7fbeab2ef2a5ab346445f2d6960215427f8e8edc
Alvin Savoy <asavoy@users.noreply.github.com>
2021-08-04 15:14:28 +1000

    Don't use keymap.json cos it autocompiles

--- json-to-asavoy-corne-mx.sh
+++ json-to-asavoy-corne-mx.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
+set -x
 
-qmk json2c -o keyboards/crkbd/keymaps/asavoy-corne-mx/keymap.c keyboards/crkbd/keymaps/asavoy-corne-mx/keymap.json
+qmk json2c -o keyboards/crkbd/keymaps/asavoy-corne-mx/keymap.c keyboards/crkbd/keymaps/asavoy-corne-mx/asavoy-corne-mx.json
+
+sed -I '' 's/LAYOUT_split_3x6_3/LAYOUT/g' keyboards/crkbd/keymaps/asavoy-corne-mx/keymap.c
 
--- pull-asavoy-corne-mx.sh
+++ pull-asavoy-corne-mx.sh
@@ -2,4 +2,5 @@
 set -e
 set -x
 
-cp /Users/alvin/Downloads/asavoy-corne-mx.json keyboards/crkbd/keymaps/asavoy-corne-mx/keymap.json
+# NOTE: don't copy to keymap.json as QMK will find it and json2c it on compile
+mv /Users/alvin/Downloads/asavoy-corne-mx.json keyboards/crkbd/keymaps/asavoy-corne-mx/asavoy-corne-mx.json
